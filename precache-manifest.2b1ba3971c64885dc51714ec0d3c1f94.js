fabb5e9ce3a98cdb3cae759ff8e0deee131ef256
pyankovit <pyankov.it@gmail.com>
2019-06-10 18:45:34 +0300

    Updates

---
+++ precache-manifest.2b1ba3971c64885dc51714ec0d3c1f94.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3e16ee1f52e8c870b6ba2bcc368408",
+    "url": "/miushi.github.io/static/media/Set6.bf3e16ee.jpg"
+  },
+  {
+    "revision": "bd91e67debaf9afd9b5b",
+    "url": "/miushi.github.io/static/css/main.62a0127c.chunk.css"
+  },
+  {
+    "revision": "f9e90fbd697a8fda3b7b",
+    "url": "/miushi.github.io/static/js/runtime~main.f9e90fbd.js"
+  },
+  {
+    "revision": "f673ecf906861ca65242",
+    "url": "/miushi.github.io/static/js/2.f673ecf9.chunk.js"
+  },
+  {
+    "revision": "388f1f1ece31b2d009c1d1b925522d92",
+    "url": "/miushi.github.io/static/media/Set8.388f1f1e.jpg"
+  },
+  {
+    "revision": "422b4cb0494b20e780ce2a16726c7b66",
+    "url": "/miushi.github.io/static/media/Set10.422b4cb0.jpg"
+  },
+  {
+    "revision": "8f015b5f40835948dbc45a0a096e6543",
+    "url": "/miushi.github.io/static/media/Set5.8f015b5f.jpg"
+  },
+  {
+    "revision": "bd91e67debaf9afd9b5b",
+    "url": "/miushi.github.io/static/js/main.bd91e67d.chunk.js"
+  },
+  {
+    "revision": "4769e0ef26ced8e11b25e9dbfe11270e",
+    "url": "/miushi.github.io/static/media/formContacts.4769e0ef.jpg"
+  },
+  {
+    "revision": "1aef7426aa5b2386a4a54a6264af79c8",
+    "url": "/miushi.github.io/static/media/foodphoto.1aef7426.png"
+  },
+  {
+    "revision": "fe3bbcecd335a7926b0d6cf2643ed607",
+    "url": "/miushi.github.io/static/media/photo.fe3bbcec.png"
+  },
+  {
+    "revision": "97fce2413e6d981923086c51575f9046",
+    "url": "/miushi.github.io/static/media/about.97fce241.png"
+  },
+  {
+    "revision": "e0b5b84867b053424a5e458d07d6b0a1",
+    "url": "/miushi.github.io/static/media/logo.e0b5b848.svg"
+  },
+  {
+    "revision": "6b14aa9a6cc0ed90c461e45f103bfff2",
+    "url": "/miushi.github.io/index.html"
+  }
+];
